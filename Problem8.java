ff1d834b25b37ca27a3e521bda49c7d7a56de12e
sithu31 <sithesh.ve2023cse@sece.ac.in>
2026-06-15 18:52:53 +0530

    Assignment8

---
+++ Problem8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Problem8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Input a binary number: ");
+        String binaryNumber = sc.nextLine();
+        
+        int decimalNumber = Integer.parseInt(binaryNumber, 2);
+        
+        System.out.println("Decimal Number: " + decimalNumber);
+        
+        sc.close();
+    }
+}
